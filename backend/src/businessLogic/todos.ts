2b1fcabbad044e47d6bb40c1d1f02742b3c51d2b
Ibrahim Olagoke <olagokeibrahim165@gmail.com>
2023-02-13 11:31:58 +0100

    Implement get-todo and createtodo validation

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -12,6 +12,11 @@ const logger = createLogger('TodosAccess')
 const attachmentUtils = new AttachmentUtils()
 const todosAccess = new TodosAccess()
 
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+  logger.info('Get todos for user function called')
+  return todosAccess.getAllTodos(userId)
+}
+
 export async function createTodo(
   newTodo: CreateTodoRequest,
   userId: string
